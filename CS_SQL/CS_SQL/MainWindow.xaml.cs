1f4148cc2e74282280e3971009fb9ec93e16aed1
TARITPETNU <56666174+TARITPETNU@users.noreply.github.com>
2020-12-15 13:40:49 +0700

    SQLSIMPLE

---
+++ CS_SQL/CS_SQL/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_SQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataAccess.InitializeDatabase();
+        }
+
+        private void insert_Click(object sender, RoutedEventArgs e)
+        {
+            dataAccess.AddData(0, "T", "P", "R");
+        }
+
+        private void showBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string data = "";
+            foreach (string inside in dataAccess.GetData())
+            {
+                data = data + " " + inside;
+            }
+            MessageBox.Show(data);
+            
+        }
+    }
+}
---
+++ CS_SQL/CS_SQL/dataAccess.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_SQL
+{
+    class dataAccess
+    {
+        public static void InitializeDatabase()
+        {
+
+
+            using (SqliteConnection db = new SqliteConnection("Filename=Customers.db"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " + "EXISTS Customers (UID INTEGER PRIMARY KEY, " +" firstName NVARCHAR(2048), " + "lastName NVARCHAR(20), " + "Email NVARCHAR(100) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+        public static void AddData(int ID, string name, string lastname, string Email)
+        {
+              using (SqliteConnection db =
+              new SqliteConnection("Filename=Customers.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO Customers VALUES (@UID, @firstName, @lastName, @Email);";
+                insertCommand.Parameters.AddWithValue("@UID", ID);
+                insertCommand.Parameters.AddWithValue("@firstName", name);
+                insertCommand.Parameters.AddWithValue("@lastName", lastname);
+                insertCommand.Parameters.AddWithValue("@Email", Email);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();   
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=Customers.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from Customers", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                    entries.Add(query.GetString(3));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
